b13a89d9dbdd6f9fd795a535661c7516099c9775
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-06-06 12:26:08 +0600

    Create single page application and React Router

---
+++ react2/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
+
+import './App.css';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+function App() {
+  return (
+   <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
