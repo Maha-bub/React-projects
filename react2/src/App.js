ae10a445b87131dd32aa7e6da12c1f11b3bc4244
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-06-06 14:11:30 +0600

    Create single page application and React Router

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,27 +1,11 @@
-import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
-
+import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
 
 function App() {
   return (
-   <BrowserRouter>
-      {/* Navigation */}
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/contact">Contact</Link>
-      </nav>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="container">
+     <h1 className='text-warning 2xl text-center'>Welcome to React bootstrap v5</h1>
+    </div>
   );
 }
 
---
+++ react2/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <>
+            <div class="p-5 bg-primary text-white text-center">
+                <h1>My First Bootstrap 5 Page</h1>
+                <p>Resize this responsive page to see the effect!</p>
+            </div>
+
+
+
+        </>
+    )
+}
