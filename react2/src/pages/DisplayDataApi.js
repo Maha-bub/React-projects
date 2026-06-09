a3f72750006006d28a896960ac59d605eb0b78bb
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-06-09 11:44:56 +0600

    Display data form api

--- react2/src/pages/DisplayDataApi.js
+++ react2/src/pages/DisplayDataApi.js
@@ -7,12 +7,38 @@ import Nav from '../components/Nav'
 
 
 export default function DisplayDataApi() {
+    const [users, setUsers] = useState([])
+
+    useEffect(
+        () => {
+            fetch('https://jsonplaceholder.typicode.com/users')
+                .then(res => res.json())
+                .then(data => setUsers(data));
+        }, [])
+
+    console.log(users)
     return (
         <>
             <Header />
             <Nav />
             <h4>Display Api data using axios</h4>
+            <div className='row'>
+                {
+                    users.map(user => (
+                        <div key={user.id} className='col-md-3 mb-4'>
+                            <div className='card h-100 shadow-sm'>
+                                <div className='card-body'>
+                                    <p><strong>Name:</strong> {user.name}</p>
+                                    <p><strong>Email:</strong> {user.email}</p>
+                                    <p><strong>Organization:</strong> {user.company.name}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
             <Footer />
+
 
         </>
     )
