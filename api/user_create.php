05d930e12fd70fa3fff62009f641fff6309642fe
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-06-11 13:05:22 +0600

    Display data form api

--- api/user_create.php
+++ api/user_create.php
@@ -1,20 +1,24 @@
 <?php
 include_once("dbconfig.php");
 
-$data=json_decode(file_get_contents('php://input'),true);
+$data = json_decode(file_get_contents('php://input'), true);
 
 // user data store to variables
-$name=$data['firstname'];
-$gender=$data['gender'];
-$district=$data['district'];
-$address=$data['address'];
+$name = $data['firstname'];
+$gender = $data['gender'];
+$district = $data['district'];
+$address = $data['address'];
+if (!empty($name)) {
+    $db->query("INSERT INTO users (id, name,address,gender, district) VALUES(NULL,'$name','$address','$gender','$district')");
 
-$db->query("INSERT INTO users (id, name,address,gender, district) VALUES(NULL,'$name','$address','$gender','$district')");
 
-if($db->affected_rows>0){
-    echo json_encode(["message"=>"User created successfully"]);
-}else{
-    echo json_encode(["message"=>"Something wrong!!!"]);
+}
+
+
+if ($db->affected_rows > 0) {
+    echo json_encode(["message" => "User created successfully"]);
+} else {
+    echo json_encode(["message" => "Something wrong!!!"]);
 }
 
 
