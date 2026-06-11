46342997e289e5342af3c615d6e32c06649027f6
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-06-11 12:39:02 +0600

    REST API

---
+++ api/user_create.php
@@ -0,0 +1,22 @@
+<?php
+include_once("dbconfig.php");
+
+$data=json_decode(file_get_contents('php://input'),true);
+
+// user data store to variables
+$name=['firstname'];
+$gender=['gender'];
+$district=['district'];
+$address=['address'];
+
+$db->query("INSERT INTO users (id, name,address,gender, district) VALUES(NULL,'$name','$address','$gender','$district')");
+
+if($db->affected_rows>0){
+    echo json_encode(["message"=>"User created successfully"]);
+}else{
+    echo json_encode(["message"=>"Something wrong!!!"]);
+}
+
+
+
+?>
--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -2,18 +2,11 @@ import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Nav from '../components/Nav'
+import axios from 'axios'
 
 
 export default function Forms() {
 
-  // single input field value handle functions
-
-
-  // const [name, setName]=useState("");
-  // console.log(name);
-
-  // function handleChange(e){
-  //   setName(e.target.value);
 
 
 
@@ -35,6 +28,17 @@ export default function Forms() {
     setDistrict(event.target.value);
   }
 
+  // handleSubmit Function for post data to user_info database
+  function handleSubmit(e) {
+    e.preventDefault();
+    const response= axios.post("http://localhost/react2/api/user_create.php", inputs)
+    .then(res=>{alert("data submit successfully done!",res.message)});
+
+
+
+  }
+
+
 
 
   return (
@@ -46,7 +50,7 @@ export default function Forms() {
 
 
       <form>
-        <label>FullName:
+        <label>FullName:<br />
           <input
             type="text"
             name="firstname"
@@ -54,7 +58,19 @@ export default function Forms() {
             onChange={handleChange}
           />
         </label> <br /> <br />
-        <label>Address:
+        <h5>Your Gender:</h5>
+        Male: <input type="radio" name='gender' value="Male" checked={inputs.gender === 'Male'} onChange={handleChange} />
+        Female: <input type="radio" name='gender' value="Female" checked={inputs.gender === 'Female'} onChange={handleChange} />
+        <h2>Select a District:</h2>
+        <select name="district" value={district} onChange={handleChange2} id="">
+
+          <option value="Dhaka">Dhaka</option>
+          <option value="Rangpur">Rangpur</option>
+          <option value="Mymonsing">Mymonsing</option>
+          <option value="Sylhet">Sylhet</option>
+          <option value="Barisal">Barisal</option>
+        </select><br /><br />
+        <label>Address:<br />
           <textarea name="address" onChange={handleChange} id="">{inputs.address}</textarea>
           {/* <input
             type="text"
@@ -63,20 +79,12 @@ export default function Forms() {
             onChange={handleChange}
           /> */}
         </label> <br />
-        <h4>Your Gender:</h4>
-        Male: <input type="radio" name='gender' value="Male" checked={inputs.gender === 'Male'} onChange={handleChange} />
-        Female: <input type="radio" name='gender' value="Female" checked={inputs.gender === 'Female'} onChange={handleChange} />
+
         <h3>Current values:</h3>
         <p> {inputs.firstname} comes from  {inputs.address}</p> <br />
-        <h2>Select a District:</h2> <br />
-        <select name="district" value={district} onChange={handleChange2} id="">
 
-          <option value="Dhaka">Dhaka</option>
-          <option value="Rangpur">Rangpur</option>
-          <option value="Mymonsing">Mymonsing</option>
-          <option value="Sylhet">Sylhet</option>
-          <option value="Barisal">Barisal</option>
-        </select>
+        <button onClick={handleSubmit} type="submit">Submit</button>
+
       </form>
 
 
